df206d792446e81e7b45a9fea058dda42abb42c9
Shreyas Rawat <shreyas1rawat@gmail.com>
2024-03-31 17:06:17 -0700

    First working version

--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 from donkeycar.parts.actuator import PulseController
 from donkeycar.parts import pins
+import time
 
 pwm_pin = 'PCA9685.1:40.0'
 pwmFreq = 60
@@ -19,13 +20,17 @@ print()
 
 while True:
     try:
-        val = input(input_prompt)
-        if val == 'q' or val == 'Q':
-            break
-        pmw = int(val)
-        car.run(pmw)
+        #val = input(input_prompt)
+        #if val == 'q' or val == 'Q':
+            #break
+        #pmw = int(val)
+        car.run(10)
+        time.sleep(1)
+        car.run(395)
     except KeyboardInterrupt:
         print("\nKeyboardInterrupt received, exit.")
         break
     except Exception as ex:
         print(f"Oops, {ex}")
+
+car.run(0)
